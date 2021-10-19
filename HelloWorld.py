011d20dd8cbf890c29a680bb80fc2041addab3aa
AngyUg <proofmyself@gmail.com>
2021-10-19 23:59:18 +0300

    Initial commit

---
+++ HelloWorld.py
@@ -0,0 +1,6 @@
+class HelloWorld:
+
+    @staticmethod
+    def hello_world_function():
+        hello_world_string = "Hello group 22 from class HelloWorld"
+        print(f"{hello_world_string}")
---
+++ main.py
@@ -0,0 +1,15 @@
+import HelloWorld
+
+
+def hello_world_function():
+    hello_world_string = "Hello group 22 function"
+    print(f"{hello_world_string}")
+
+
+hello_world_function()
+
+# or simple
+print("Simle Hello group 22")
+
+# or from class
+HelloWorld.HelloWorld.hello_world_function()
